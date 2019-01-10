2ad00bbd8f38c70afc74ec593cbf55be2b180023
francescolami <francesco.lami@phd.unipd.it>
2019-01-10 12:45:02 +0100

    new file, I guess?

---
+++ ok.R
@@ -0,0 +1,26 @@
+---
+title: "wat is dis"
+output: html_document
+---
+
+```{r setup, include=FALSE}
+knitr::opts_chunk$set(echo = TRUE)
+```
+
+## R Markdown
+
+This is an R Markdown document. Markdown is a simple formatting syntax for authoring HTML, PDF, and MS Word docuBLEEEEEERKchunks within the document. You can embed an R code chunk like this:
+
+```{r cars}
+summary(cars)
+```
+
+## Including Plots
+
+You can also embed plots, for example:
+
+```{r pressure, echo=FALSE}
+plot(pressure)
+```
+
+Note that the `echo = FALSE` parameter was added to the code chunk to prevent printing of the R code that generated the plot.
